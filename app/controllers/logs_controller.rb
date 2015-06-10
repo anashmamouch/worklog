48caac7b89df5df131ce3fa3193cfe88ff6cc6fc
Anas Hmamouch <anashmamouch90@gmail.com>
2015-06-10 20:23:02 +0100

    enhancing performance

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -2,6 +2,7 @@ class LogsController < ApplicationController
 
 	def index
 		@logs = Log.paginate(:page => params[:page],:per_page => 10).order('created_at DESC')
+		fresh_when @logs
 	end
 
 	def create
